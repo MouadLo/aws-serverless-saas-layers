301cc82bacfb1af25a019e16327dadb6912e848a
EC2 Default User <ec2-user@ip-172-31-22-8.ec2.internal>
2020-02-27 05:24:18 +0000

    correction Lab4

--- Lab4/server/order-manager/order-manager-dal.js
+++ Lab4/server/order-manager/order-manager-dal.js
@@ -5,54 +5,54 @@
 
 'use strict';
 
+const partitionManager = require('/opt/nodejs/partition-manager.js');
+const tokenManager = require('/opt/nodejs/token-manager.js');
 const logManager = require('/opt/nodejs/log-manager.js');
-const helper = require('/opt/nodejs/helper.js');
 const metricsManager = require('/opt/nodejs/metrics-manager.js');
 const doc = require('dynamodb-doc');
 const dynamodb = new doc.DynamoDB();
 
 const tableName = "Order";
-const tableDefinition = {
-    AttributeDefinitions: [ 
-    {
-      AttributeName: "OrderId", 
-      AttributeType: "S"
-    } ], 
-    KeySchema: [ 
-    {
-      AttributeName: "OrderId", 
-      KeyType: "HASH"
-    } ], 
-    ProvisionedThroughput: {
-     ReadCapacityUnits: 5, 
-     WriteCapacityUnits: 5
-    }, 
-    TableName: tableName
-};
 
 // Get an order from DynamoDB
 module.exports.getOrder = function(event, callback) {
     logManager.log(event, "OrderManager", { "Message": "DAL GetOrder() called.", "OrderId" : event.pathParameters.resourceId});
-
+    
     const start = new Date().getTime();
-    const params = {
-        "TableName": tableName,
-        "Key": {
-            OrderId: event.pathParameters.resourceId
+    const tenantId = tokenManager.getTenantId(event);
+    partitionManager.getPartition(event, partitionManager.ORDER_MANAGER, event.pathParameters.resourceId, function(tableName) {
+        let params;
+        if (tableName.indexOf('Pooled') >=0){
+            params = {
+                "TableName": tableName,
+                "Key": {
+                    TenantId: tenantId,
+                    OrderId: event.pathParameters.resourceId
+                }
+            };
+        } else {
+            params = {
+                "TableName": tableName,
+                "Key": {
+                    OrderId: event.pathParameters.resourceId
+                }
+            }
         }
-    };
 
-    dynamodb.getItem(params, (err, data) => {
-        let response;
-        if (err)
-            response = createResponse(500, err);
-        else
-            response = createResponse(200, data.Item ? data.Item.doc : null);
+        dynamodb.getItem(params, (err, data) => {
+            let response;
+            if (err){
+                console.log(err);
+                response = createResponse(500, err);
+            }
+            else
+                response = createResponse(200, data.Item ? data.Item.doc : null);
 
-        const end = new Date().getTime();
-        metricsManager.recordMetricEvent(event, "OrderManager", "GetOrder", event, end - start);
-        callback(response);
-    });    
+            const end = new Date().getTime();
+            metricsManager.recordMetricEvent(event, "OrderManager", "GetOrder", event, end - start);
+            callback(response);
+        });   
+    }); 
 };
 
 // Add or update an order to DynamoDB 
@@ -60,8 +60,10 @@ module.exports.updateOrder = (event, callback) => {
     logManager.log(event, "OrderManager", {"Message": "DAL UpdateOrder() called.", "OrderId" : event.pathParameters.resourceId});
 
     const start = new Date().getTime();
-    helper.createTable(tableDefinition, function() {
+    const tenantId = tokenManager.getTenantId(event);
+    partitionManager.getPartition(event, partitionManager.ORDER_MANAGER, event.pathParameters.resourceId, function(tableName) {
         const item = {
+            "TenantId": tenantId,
             "OrderId": event.pathParameters.resourceId,
             "doc": event.body
         };
@@ -73,8 +75,10 @@ module.exports.updateOrder = (event, callback) => {
 
         dynamodb.putItem(params, (err) => {
             let response;
-            if (err)
+            if (err){
+                console.log(err);
                 response = createResponse(500, err);
+            }
             else
                 response = createResponse(200, null);
             
@@ -90,23 +94,39 @@ module.exports.deleteOrder = (event, callback) => {
     logManager.log(event, "OrderManager", {"Message": "DAL deleteOrder() called.", "OrderId" : event.pathParameters.resourceId});
     
     const start = new Date().getTime();
-    const params = {
-        "TableName": tableName,
-        "Key": {
-            "OrderId": event.pathParameters.resourceId
+    const tenantId = tokenManager.getTenantId(event);
+    partitionManager.getPartition(event, partitionManager.ORDER_MANAGER, event.pathParameters.resourceId, function(tableName) {
+        let params;
+        if (tableName.indexOf('Pooled') >=0){
+            params = {
+                "TableName": tableName,
+                "Key": {
+                    TenantId: tenantId,
+                    OrderId: event.pathParameters.resourceId
+                }
+            };
+        } else {
+            params = {
+                "TableName": tableName,
+                "Key": {
+                    OrderId: event.pathParameters.resourceId
+                }
+            }
         }
-    };
 
-    dynamodb.deleteItem(params, (err) => {
-        let response;
-        if (err)
-            response = createResponse(500, err);
-        else
-            response = createResponse(200, null);
-        
-        const end = new Date().getTime();
-        metricsManager.recordMetricEvent(event, "OrderManager", "DeleteOrder", event, end - start);
-        callback(response);
+        dynamodb.deleteItem(params, (err) => {
+            let response;
+            if (err){
+                console.log(err);
+                response = createResponse(500, err);
+            }
+            else
+                response = createResponse(200, null);
+            
+            const end = new Date().getTime();
+            metricsManager.recordMetricEvent(event, "OrderManager", "DeleteOrder", event, end - start);
+            callback(response);
+        });
     });
 };
 
